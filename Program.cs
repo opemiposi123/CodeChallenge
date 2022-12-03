4821007e4f4fabc52d5c4a11b42cb5033c660e60
opemiposi123 <abdulwaheedmahmoodahmad@gmail.com>
2022-12-03 01:10:14 +0100

    Getting the substring

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodeChallenge
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+
+
+            string word = "AfricanJournal";
+            string a = word.Substring(0, word.Length / 2);
+            string b = word.Substring(word.Length / 2);
+
+           
+            
+
+        
+ 
+
+        }
+    }
+}
+
+
+
+
