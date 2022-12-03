540b2fdc266920f0d41f99d3a9a0714a35ae2bab
opemiposi123 <abdulwaheedmahmoodahmad@gmail.com>
2022-12-03 01:10:53 +0100

    Reversing the substring

--- Program.cs
+++ Program.cs
@@ -13,7 +13,19 @@ namespace CodeChallenge
             string a = word.Substring(0, word.Length / 2);
             string b = word.Substring(word.Length / 2);
 
-           
+            char[] aArray = a.ToCharArray();
+            string aReverse = String.Empty;
+            for (int i = aArray.Length - 1; i > -1; i--)
+            {
+                aReverse += aArray[i];
+            }
+
+            char[] bArray = b.ToCharArray();
+            string bReverse = String.Empty;
+            for (int i = bArray.Length - 1; i > -1; i--)
+            {
+                bReverse += bArray[i];
+            }
             
 
         
